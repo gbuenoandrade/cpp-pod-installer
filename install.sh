3d967f87180cb033302ca2be36ebcffe040b212b
Guilherme Andrade <guilherme.andrade@tfgco.com>
2017-04-12 00:50:53 -0300

    first working version

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo 'hey dudeee'
+
+
+rm -rf ~/Applications/cpp-pod
+
+git clone --depth=1 git@git.topfreegames.com:guilherme.andrade/cpp-pod.git ~/Applications/cpp-pod
+git clone --depth=1 git@git.topfreegames.com:guilherme.andrade/cpp-specs.git ~/Applications/cpp-pod/cpp-specs
+bundle install --gemfile ~/Applications/cpp-pod/Gemfile
+
+mkdir -p /usr/local/bin/
+rm -f /usr/local/bin/cpp-pod
+ln -s ~/Applications/cpp-pod/main.rb /usr/local/bin/cpp-pod
+chmod +x /usr/local/bin/cpp-pod
